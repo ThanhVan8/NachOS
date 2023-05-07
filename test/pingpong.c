d24dae7a6627d3912794ce240391ff329bacd6e4
Thanh Van <tttvan21@clc.fitus.edu.vn>
2023-05-07 18:54:29 +0700

    upload

--- test/pingpong.c
+++ test/pingpong.c
@@ -4,12 +4,14 @@
 int main()
 {
 	int pingPID, pongPID;
+	int ecPing, ecPong; // exit code
 
 	pingPID = Exec("./test/ping");
 	pongPID = Exec("./test/pong");
 
-	Join(pingPID);
-	Join(pongPID);
-	
-	Exit(0);
+	ecPing = Join(pingPID);
+	ecPong = Join(pongPID);
+
+	Exit(ecPing);
+	Exit(ecPong);
 }
